ff89ecf34c2bff795cdeb77441fd490bd53520d8
Tom Vincent <tlvince+linux@gmail.com>
2010-09-04 15:33:03 +0800

    Updated GPL3 header

--- isSet.sh
+++ isSet.sh
@@ -1,4 +1,23 @@
 ##
+# Functions relating to variable setting.
+#
+# Copyright 2010 Tom Vincent <http://www.tlvince.com/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+##
 # If the given variable is not set, prompt to set it.
 #
 # @param $1 string  The name of a variable.
