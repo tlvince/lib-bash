1069e43d0d0742928f992f4cd2d71fde24023fe5
Tom Vincent <tlvince+linux@gmail.com>
2010-09-04 12:53:35 +0800

    New library functions

---
+++ isSet.sh
@@ -0,0 +1,10 @@
+##
+# If the given variable is not set, prompt to set it.
+#
+# @param $1 string  The name of a variable.
+# @param $2 string  An arbitrary prompt, corresponding to $1.
+#
+isSet()
+{
+    [[ -z ${!1} ]] && read -p "$2" $1 && isSet "$@"
+}
