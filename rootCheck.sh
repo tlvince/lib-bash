2f7ad56ee86c61df8c05f94cd2a25de24a77cc1e
Tom Vincent <tlvince+linux@gmail.com>
2010-09-04 12:52:41 +0800

    Parameter expansion

--- rootCheck.sh
+++ rootCheck.sh
@@ -1,12 +1,10 @@
-#
-# Check if the current user is root.
-#
-# Version:  0.1.2 (2010-04-25)
+##
+# Check if the current user is root, otherwise exit.
 #
 rootCheck()
 {
     if [[ $EUID -ne 0 ]]; then
-        echo "${NAME:-$(basename $0)} must be run as root" 1>&2
+        echo "${0##*/} must be run as root" >&2
         exit 1
     fi
 }
