2ff5061c96f7efd01da3e07b5e758bc7c497fbec
Tom Vincent <tlvince+linux@gmail.com>
2010-12-01 00:43:39 +0800

    Compromise simplicity for better filtering

--- spellChecker.sh
+++ spellChecker.sh
@@ -1,5 +1,11 @@
 # In-line spellchecker
 sp()
 {
-    aspell -a < <(echo $@) | grep -v "^@(#)"
+    local words
+    which "aspell" > /dev/null 2>&1 || exit 1
+
+    words=$(aspell -a < <(echo $@) | grep -v "^@(#)")   # grep header
+    [[ $words == "*" ]] && return   # correct spelling
+
+    sed -e 's/.*:\ //' < <(echo $words) # rid of the crap
 }
